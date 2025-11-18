0ec7066714b0810bb25c8603fa9c45a446e91e8b
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-11-18 10:19:26 -0300

    refactor: Change transactionId parameter type to UUID in TransactionRepository

--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Repository/TransactionRepository.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Repository/TransactionRepository.java
@@ -1,13 +1,18 @@
 package com.matheus.payments.instant.Infra.Repository;
 
 import com.matheus.payments.instant.Domain.Transaction.Transaction;
+import com.matheus.payments.instant.Domain.Transaction.TransactionOutbox;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
 
-    Optional<Transaction> findByTransactionId(String transactionId);
+    Optional<Transaction> findByTransactionId(UUID transactionId);
+
 }
