a8fd9ecd997e9671f438f06a69485c728f11af55
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-11-21 13:13:19 -0300

    feat: Implement SSL configuration for secure communication in WalletServer

--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Http/Clients/WalletServer.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Http/Clients/WalletServer.java
@@ -1,31 +1,51 @@
 package com.matheus.payments.instant.Infra.Http.Clients;
 
 
-import com.matheus.payments.instant.Application.DTOs.Response.PaymentProcessorResponse;
-import com.matheus.payments.instant.Infra.Exceptions.Custom.FailedToSentException;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.KeyStore;
 
 @Service
 public class WalletServer {
 
+    private final HttpClient responseClient;
+
+    public WalletServer() throws Exception {
+        // Carrega o truststore
+        KeyStore trustStore = KeyStore.getInstance("JKS");
+        try (InputStream trustStream = new ClassPathResource("Certifications/truststore.jks").getInputStream()) {
+            trustStore.load(trustStream, "changeit".toCharArray());
+        }
+
+        // Cria o SSLContext que confia nesse truststore
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(trustStore);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+
+        // Cria o HttpClient com SSL customizado
+        this.responseClient = HttpClient.newBuilder()
+                .sslContext(sslContext)
+                .build();
+    }
 
     public HttpResponse<String> instantPaymentRequest(String jsonPayload) throws IOException, InterruptedException {
-
-
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8081/wallet/instant-payment"))
+                .uri(URI.create("https://localhost:8081/wallets/instant-payment"))
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
                 .build();
 
-        HttpClient responseClient = HttpClient.newHttpClient();
         return responseClient.send(request, HttpResponse.BodyHandlers.ofString());
-
     }
 }
