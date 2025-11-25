59287f987cb850a5bcbb7f367b7b67820cc68e0e
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-11-25 14:50:58 -0300

    feat: Refactor WalletService to enhance wallet creation and payment processing logic

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Application/WalletService.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Application/WalletService.java
@@ -1,65 +1,82 @@
 package com.matheus.payments.wallet.Application;
 
+import com.matheus.payments.wallet.Application.DTOs.Request.CreateWalletRequest;
 import com.matheus.payments.wallet.Application.DTOs.Request.TransactionDTO;
-import com.matheus.payments.wallet.Application.DTOs.Response.PaymentProcessorResponse;
+import com.matheus.payments.wallet.Application.DTOs.Response.InstantPaymentResponse;
 import com.matheus.payments.wallet.Domain.Wallet.Wallet;
-import com.matheus.payments.wallet.Domain.Wallet.accountType;
+import com.matheus.payments.wallet.Domain.Wallet.WalletKeys;
 import com.matheus.payments.wallet.Infra.Exceptions.Custom.InsuficientBalanceException;
+import com.matheus.payments.wallet.Infra.Exceptions.Custom.KeyValueAlreadyExists;
 import com.matheus.payments.wallet.Infra.Exceptions.Custom.SameUserException;
 import com.matheus.payments.wallet.Infra.Exceptions.Custom.WalletNotFoundException;
+import com.matheus.payments.wallet.Infra.Repository.WalletKeysRepository;
 import com.matheus.payments.wallet.Infra.Repository.WalletRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 @Service
 public class WalletService {
 
-    private WalletRepository walletRepository;
+    private final WalletRepository walletRepository;
+    private final WalletKeysRepository walletKeysRepository;
 
-    public WalletService(WalletRepository walletRepository) {
+    public WalletService(WalletRepository walletRepository, WalletKeysRepository walletKeysRepository) {
+        this.walletKeysRepository = walletKeysRepository;
         this.walletRepository = walletRepository;
     }
 
+    @Transactional
+    public boolean createWallet(CreateWalletRequest request) {
 
-    public HttpStatus createWallet(UUID userId, accountType accountType) {
+        System.out.println("Creating wallet for user: " + request.keyValue);
 
-        System.out.println("Creating wallet for user: " + userId);
+        boolean keyExists = walletKeysRepository.existsWalletKeysByKeyValue(request.keyValue);
 
-        Wallet wallet = new Wallet(userId, accountType);
+        if (keyExists) {
+            throw new KeyValueAlreadyExists("Key value already exists: " + request.keyValue);
+        }
+
+        Wallet wallet = new Wallet(request.accountType);
         walletRepository.save(wallet);
-        return HttpStatus.CREATED;
+
+        WalletKeys walletKeys = new WalletKeys(request.keyValue, request.keyType, wallet.getAccountId());
+        walletKeysRepository.save(walletKeys);
+
+        return true;
     }
 
     @Transactional
-    public PaymentProcessorResponse handlePaymentProcessor(TransactionDTO request){
+    public InstantPaymentResponse handlePaymentProcessor(TransactionDTO request) {
 
-        var payloadResponse = new PaymentProcessorResponse(
-                request.getTransactionId(),
-                true,
-                false,
-                null
-        );
+        WalletKeys accountIdSender;
+        WalletKeys accountIdReceiver;
+        Wallet senderWallet = null;
+        Wallet receiverWallet = null;
 
         try {
+            accountIdSender = walletKeysRepository.findwalletIdBykey(request.getSenderKey())
+                    .orElseThrow(() -> new WalletNotFoundException("Sender wallet key not found"));
+
+            accountIdReceiver = walletKeysRepository.findwalletIdBykey(request.getReceiverKey())
+                    .orElseThrow(() -> new WalletNotFoundException("Receiver wallet key not found"));
 
             // Same user validation
-            if (request.getSenderId().equals(request.getReceiverId())) {
-                throw new SameUserException("Sender and receiver cannot be the same");
+            if (accountIdSender.getWalletId().equals(accountIdReceiver.getWalletId())) {
+                throw new SameUserException("Sender and Receiver cannot be the same");
             }
 
-            Wallet senderWallet = walletRepository.findByUserIdAndIsActiveTrue(request.getSenderId())
+            senderWallet = walletRepository.findByAccountIdAndIsActiveTrue(accountIdSender.getWalletId())
                     .orElseThrow(() -> new WalletNotFoundException("Sender wallet not found or inactive"));
+
+            receiverWallet = walletRepository.findByAccountIdAndIsActiveTrue(accountIdReceiver.getWalletId())
+                    .orElseThrow(() -> new WalletNotFoundException("Receiver wallet not found or inactive"));
 
             // Sufficient balance validation (-1 (invalid), 0 (equal), 1 (sufficient))
             if (senderWallet.getBalance().compareTo(request.getAmount()) < 0) {
                 throw new InsuficientBalanceException("Insufficient funds in sender's wallet");
             }
-            Wallet receiverWallet = walletRepository.findByUserIdAndIsActiveTrue(request.getReceiverId())
-                    .orElseThrow(() -> new WalletNotFoundException("Receiver wallet not found or inactive"));
 
             BigDecimal amount = request.getAmount();
 
@@ -68,17 +85,22 @@ public class WalletService {
 
             walletRepository.save(senderWallet);
             walletRepository.save(receiverWallet);
-        } catch (Exception e)
-        {
-            payloadResponse.setIsFailed(true);
-            payloadResponse.setIsSucessful(false);
-            payloadResponse.setFailedMessage(e.getMessage());
 
-            return payloadResponse;
+        } catch (Exception e) {
+            return switch (e) {
+                case WalletNotFoundException walletNotFoundException ->
+                        new InstantPaymentResponse(false, null, null, e.getMessage());
+                case SameUserException sameUserException ->
+                        new InstantPaymentResponse(false, null, null, e.getMessage());
+                case InsuficientBalanceException insuficientBalanceException ->
+                        new InstantPaymentResponse(false, senderWallet.getAccountId(), null, e.getMessage());
+                default ->
+                        new InstantPaymentResponse(false, senderWallet.getAccountId(), receiverWallet.getAccountId(), "Transaction failed during processing: " + e.getMessage());
+            };
         }
 
-        return payloadResponse;
-    }
+        return new InstantPaymentResponse(true, senderWallet.getAccountId(), receiverWallet.getAccountId());
 
+    }
 
 }
