8dca1c2650b92eed02804e256883c7dfa7d6be63
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-11-25 15:05:53 -0300

    feat: Enhance PaymentProcessorResponse with detailed response handling methods

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Application/DTOs/Response/PaymentProcessorResponse.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Application/DTOs/Response/PaymentProcessorResponse.java
@@ -1,23 +1,60 @@
 package com.matheus.payments.wallet.Application.DTOs.Response;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.UUID;
 
-@Getter @Setter
+@Getter
 public class PaymentProcessorResponse {
 
-    private UUID TransactionId;
-    private Boolean isSucessful;
+    private UUID transactionId;
+    private UUID senderAccountId;
+    private UUID receiverAccountId;
+    private Boolean isSent;
+    private Boolean isSuccessful;
     private Boolean isFailed;
     private String failedMessage;
 
-    public PaymentProcessorResponse(UUID transactionId, Boolean isSucessful, Boolean isFailed, String failedMessage) {
-        this.TransactionId = transactionId;
-        this.isSucessful = isSucessful;
-        this.isFailed = isFailed;
-        this.failedMessage = failedMessage;
+    public PaymentProcessorResponse() {
+    }
+
+    public static PaymentProcessorResponse failedResponse(UUID transactionId, UUID senderAccountId, UUID receiverAccountId, String failedMessage) {
+
+        PaymentProcessorResponse response = new PaymentProcessorResponse();
+        response.transactionId = transactionId;
+        response.isSent = true;
+        response.isFailed = true;
+        response.isSuccessful = false;
+        response.senderAccountId = senderAccountId;
+        response.receiverAccountId = receiverAccountId;
+        response.failedMessage = failedMessage;
+        return response;
+    }
+
+    public static PaymentProcessorResponse successResponse(UUID transactionId, UUID senderAccountId, UUID receiverAccountId) {
+
+        PaymentProcessorResponse response = new PaymentProcessorResponse();
+        response.transactionId = transactionId;
+        response.isSent = true;
+        response.isFailed = false;
+        response.isSuccessful = true;
+        response.senderAccountId = senderAccountId;
+        response.receiverAccountId = receiverAccountId;
+        response.failedMessage = null;
+        return response;
+    }
+
+    public static PaymentProcessorResponse connectionFailed(UUID transactionId) {
+
+        PaymentProcessorResponse response = new PaymentProcessorResponse();
+        response.transactionId = transactionId;
+        response.isSent = true;
+        response.isFailed = true;
+        response.isSuccessful = false;
+        response.senderAccountId = null;
+        response.receiverAccountId = null;
+        response.failedMessage = "Error sending payment to processor occurred while trying to reach Wallet Server. The payment could not be processed!";
+        return response;
     }
 
 
