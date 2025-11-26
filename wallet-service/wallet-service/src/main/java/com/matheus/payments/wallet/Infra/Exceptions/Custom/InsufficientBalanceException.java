052e5f7de2d85cf3af5294a606b63aada12e072f
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-11-26 14:12:49 -0300

    feat: Add InsufficientBalanceException for handling insufficient balance scenarios

---
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Exceptions/Custom/InsufficientBalanceException.java
@@ -0,0 +1,11 @@
+package com.matheus.payments.wallet.Infra.Exceptions.Custom;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+
+    public InsufficientBalanceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Exceptions/Handler/WalletExceptionHandler.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Exceptions/Handler/WalletExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.matheus.payments.wallet.Infra.Exceptions.Handler;
 
-import com.matheus.payments.wallet.Infra.Exceptions.Custom.InsuficientBalanceException;
+import com.matheus.payments.wallet.Infra.Exceptions.Custom.InsufficientBalanceException;
+import com.matheus.payments.wallet.Infra.Exceptions.Custom.KeyValueAlreadyExists;
 import com.matheus.payments.wallet.Infra.Exceptions.Custom.SameUserException;
 import com.matheus.payments.wallet.Infra.Exceptions.Custom.WalletNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -23,9 +24,15 @@ public class WalletExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(handlerMessage);
     }
 
-    @ExceptionHandler(InsuficientBalanceException.class)
-    private ResponseEntity<HandlerMessage> InsuficientBalanceException(InsuficientBalanceException insuficientBalanceException){
-        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.BAD_REQUEST , insuficientBalanceException.getMessage());
+    @ExceptionHandler(InsufficientBalanceException.class)
+    private ResponseEntity<HandlerMessage> InsufficientBalanceException(InsufficientBalanceException insufficientBalanceException){
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.BAD_REQUEST , insufficientBalanceException.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlerMessage);
+    }
+
+    @ExceptionHandler(KeyValueAlreadyExists.class)
+    private ResponseEntity<HandlerMessage> KeyValueAlreadyExists(KeyValueAlreadyExists keyValueAlreadyExists){
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.BAD_REQUEST , keyValueAlreadyExists.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlerMessage);
     }
 }
