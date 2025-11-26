c6710930110135eab075aa7dacb70c347fda2d81
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-11-26 10:14:43 -0300

    feat: Add StatementService for retrieving transaction statements by account ID

---
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Application/Services/StatementService.java
@@ -0,0 +1,27 @@
+package com.matheus.payments.instant.Application.Services;
+
+import com.matheus.payments.instant.Application.DTOs.Response.TransactionDTO;
+import com.matheus.payments.instant.Domain.Transaction.Transaction;
+import com.matheus.payments.instant.Infra.Repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+import static java.util.Arrays.stream;
+
+@Service
+public class StatementService {
+
+    private final TransactionRepository transactionRepository;
+    public StatementService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public List<Transaction> getAllTransactionsStatements(UUID accountId){
+        return transactionRepository.findTransactionsBySenderAccountIdOrReceiverAccountId(accountId, accountId)
+                .orElseThrow(() -> new RuntimeException("No transactions found for accountId: " + accountId));
+    }
+
+
+}
--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Repository/TransactionRepository.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Repository/TransactionRepository.java
@@ -1,12 +1,10 @@
 package com.matheus.payments.instant.Infra.Repository;
 
 import com.matheus.payments.instant.Domain.Transaction.Transaction;
-import com.matheus.payments.instant.Domain.Transaction.TransactionOutbox;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,5 +12,8 @@ import java.util.UUID;
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
 
     Optional<Transaction> findByTransactionId(UUID transactionId);
+
+    // Get all transaction from User by accountId (either sender or receiver)
+    Optional<List<Transaction>> findTransactionsBySenderAccountIdOrReceiverAccountId(UUID senderAccountId, UUID receiverAccountId);
 
 }
