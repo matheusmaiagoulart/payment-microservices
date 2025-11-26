ae87b0c61b44ef3ef94b238b5efdbcd3a1551f8c
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-11-26 10:15:18 -0300

    feat: Add endpoint for retrieving account transaction statements

--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Controller/TransactionController.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Controller/TransactionController.java
@@ -2,15 +2,16 @@ package com.matheus.payments.instant.Controller;
 
 import com.matheus.payments.instant.Application.DTOs.Response.PaymentProcessorResponse;
 import com.matheus.payments.instant.Application.DTOs.Request.TransactionRequest;
-import com.matheus.payments.instant.Application.Services.PaymentService;
+import com.matheus.payments.instant.Application.Services.InstantPaymentServices.PaymentService;
+import com.matheus.payments.instant.Application.Services.StatementService;
+import com.matheus.payments.instant.Domain.Transaction.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transaction")
@@ -18,6 +19,8 @@ public class TransactionController {
 
     @Autowired
     private PaymentService paymentService;
+    @Autowired
+    private StatementService statementService;
 
     @PostMapping("/pix")
     public ResponseEntity<PaymentProcessorResponse> CreateInstantPayment(@RequestBody TransactionRequest request) throws IOException, InterruptedException {
@@ -25,7 +28,10 @@ public class TransactionController {
         PaymentProcessorResponse paymentOrchestration = paymentService.paymentOrchestration(request);
 
             return ResponseEntity.ok(paymentOrchestration);
-
     }
 
+    @GetMapping("/account-statement")
+    public ResponseEntity<List<Transaction>> instantPayment(@RequestParam (name = "account", required = true)UUID account) {
+        return ResponseEntity.ok(statementService.getAllTransactionsStatements(account));
+    }
 }
