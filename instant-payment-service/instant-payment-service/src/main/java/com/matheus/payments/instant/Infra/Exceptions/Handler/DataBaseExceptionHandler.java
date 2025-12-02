cce40f4fa81f424d2beede3447d76c2078cb58ee
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-02 11:08:41 -0300

    feat: Create DataBaseExceptionHandler for implement custom exception handling

---
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Exceptions/Handler/DataBaseExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.matheus.payments.instant.Infra.Exceptions.Handler;
+
+import com.matheus.payments.instant.Infra.Exceptions.Custom.DataBaseException;
+import com.matheus.payments.instant.Infra.Exceptions.HandlerMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@RestControllerAdvice
+public class DataBaseExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<HandlerMessage> SQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException sqlIntegrityConstraintViolationException) {
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.CONFLICT, "Database constraint violation: " + sqlIntegrityConstraintViolationException.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(handlerMessage);
+    }
+
+    @ExceptionHandler(TransactionSystemException.class)
+    public ResponseEntity<HandlerMessage> TransactionSystemException(TransactionSystemException transactionSystemException) {
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.CONFLICT, "Transaction Failed: " + transactionSystemException.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(handlerMessage);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HandlerMessage> Exception(Exception e) {
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(handlerMessage);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<HandlerMessage> DataBaseException(DataBaseException dataBaseException) {
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.SERVICE_UNAVAILABLE, dataBaseException.getMessage());
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(handlerMessage);
+    }
+}
