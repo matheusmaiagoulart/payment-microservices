c6af1210e523221b433fbba2c9f33baba059e26a
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-02 11:48:36 -0300

    feat: Enhance OutboxService with auditing and custom exception handling

--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Application/Services/OutboxService.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Application/Services/OutboxService.java
@@ -1,27 +1,39 @@
 package com.matheus.payments.instant.Application.Services;
 
+import com.matheus.payments.instant.Application.Audit.OutboxServiceAudit;
 import com.matheus.payments.instant.Domain.TransactionOutbox;
+import com.matheus.payments.instant.Infra.Exceptions.Custom.DataBaseException;
 import com.matheus.payments.instant.Infra.Repository.OutboxRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.shared.Logs.LogBuilder;
 import org.springframework.stereotype.Service;
 
-@Slf4j
+/**
+ * Service class is responsible for handling Outbox operations.
+ * <p>
+ * It's a class that manage details related to Transaction and store related data about the Transaction processing.
+ */
 @Service
 public class OutboxService {
 
+    private final OutboxServiceAudit audit;
     private final OutboxRepository outboxRepository;
-    public OutboxService(OutboxRepository outboxRepository) {
+
+    public OutboxService(OutboxRepository outboxRepository, OutboxServiceAudit audit) {
+        this.audit = audit;
         this.outboxRepository = outboxRepository;
     }
 
-    public void createOutboxEntry(String transactionId, String payload)
-    {
-        log.info("Create TransactionOutbox", LogBuilder.serviceLog("/transaction/pix", "Payment",
-                transactionId, "OutboxService", "createOutboxEntry", "Payment processing started"));
+    public void createOutboxEntry(String transactionId, String payload) {
+        audit.logCreateOutbox(transactionId); // LOG
 
-        // Create Outbox Entry
-        TransactionOutbox outbox = new TransactionOutbox(transactionId, payload);
-        outboxRepository.save(outbox);
+        try {
+            // Create Outbox Entry
+            TransactionOutbox outbox = new TransactionOutbox(transactionId, payload);
+            outboxRepository.save(outbox);
+
+        } catch (DataBaseException e) {
+            audit.logErrorCreateOutbox(transactionId, e.getMessage());
+            throw new DataBaseException("An error occurred while saving Outbox Entry for transactionId: " + transactionId);
+        }
+
     }
 }
