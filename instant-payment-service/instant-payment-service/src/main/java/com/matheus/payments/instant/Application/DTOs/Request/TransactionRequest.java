ec459905ace3bb0cb80a94d85b21f31d35f867ef
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-02 12:02:04 -0300

    feat: Enhance TransactionRequest class with validation annotations for fields

--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Application/DTOs/Request/TransactionRequest.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Application/DTOs/Request/TransactionRequest.java
@@ -1,5 +1,7 @@
 package com.matheus.payments.instant.Application.DTOs.Request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -7,7 +9,17 @@ import java.math.BigDecimal;
 @Getter
 public class TransactionRequest {
 
+    private String transactionId;
+    @NotNull(message = "Sender value can't be Null") @NotEmpty(message = "Sender value can't be Empty")
     private String senderKey;
+    @NotNull(message = "Receiver value can't be Null") @NotEmpty(message = "Receiver value can't be Empty")
     private String receiverKey;
+    @NotNull(message = "Amount value can't be Null") @NotEmpty(message = "Amount value can't be Empty")
     private BigDecimal amount;
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
 }
+
+
