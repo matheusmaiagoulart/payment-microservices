dac93f5d7f7d6e58e05f027f6c2543ed011b4407
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-02 13:21:02 -0300

    feat: Refactor TransactionExceptionHandler to use RestControllerAdvice and improve exception handling methods

--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Exceptions/Handler/TransactionExceptionHandler.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Exceptions/Handler/TransactionExceptionHandler.java
@@ -1,22 +1,23 @@
 package com.matheus.payments.instant.Infra.Exceptions.Handler;
 
 import com.matheus.payments.instant.Infra.Exceptions.Custom.*;
+import com.matheus.payments.instant.Infra.Exceptions.HandlerMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-public class TransactionExceptionHandler extends TransactionAlreadySentException {
+@RestControllerAdvice
+public class TransactionExceptionHandler {
 
     @ExceptionHandler(TransactionAlreadySentException.class)
-    private ResponseEntity<HandlerMessage> TransactionAlreadySentException(TransactionAlreadySentException transactionException){
+    public ResponseEntity<HandlerMessage> TransactionAlreadySentException(TransactionAlreadySentException transactionException){
         HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.CONFLICT, transactionException.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(handlerMessage);
     }
 
     @ExceptionHandler(FailedToSentException.class)
-    private ResponseEntity<HandlerMessage> FailedToSentException(FailedToSentException failedToSentException){
+    public ResponseEntity<HandlerMessage> FailedToSentException(FailedToSentException failedToSentException){
         HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.INTERNAL_SERVER_ERROR, failedToSentException.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(handlerMessage);
     }
@@ -31,11 +32,5 @@ public class TransactionExceptionHandler extends TransactionAlreadySentException
     public ResponseEntity<HandlerMessage> TransactionNotFound(TransactionNotFound transactionNotFound) {
         HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.NOT_FOUND, transactionNotFound.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(handlerMessage);
-    }
-
-    @ExceptionHandler(CreatePaymentProcessException.class)
-    public ResponseEntity<HandlerMessage> CreatePaymentProcessException(CreatePaymentProcessException createPaymentProcessException) {
-        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.BAD_REQUEST, createPaymentProcessException.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlerMessage);
     }
 }
