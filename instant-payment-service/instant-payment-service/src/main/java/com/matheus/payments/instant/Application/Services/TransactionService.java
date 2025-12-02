36f2f874183bb156cf2c2f71c93d1984d75b1ead
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-02 13:22:36 -0300

    refactor: Add logs methods from Audit class to improve code read

--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Application/Services/TransactionService.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Application/Services/TransactionService.java
@@ -1,41 +1,57 @@
 package com.matheus.payments.instant.Application.Services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.matheus.payments.instant.Application.Audit.TransactionServiceAudit;
 import com.matheus.payments.instant.Application.DTOs.Request.TransactionRequest;
 import com.matheus.payments.instant.Application.Mappers.TransactionMapper;
 import com.matheus.payments.instant.Domain.Transaction;
+import com.matheus.payments.instant.Infra.Exceptions.Custom.DataBaseException;
 import com.matheus.payments.instant.Infra.Repository.TransactionRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.shared.Logs.LogBuilder;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 
-@Slf4j
+/**
+ * Service class is responsible for handling Transaction operations.
+ * <p>
+ * It's starting the process of creating a new Transaction
+ *
+ * @author Matheus Maia Goulart
+ */
+
 @Service
 public class TransactionService {
 
-    private final TransactionRepository transactionRepository;
+    private final TransactionServiceAudit audit;
     private final TransactionMapper transactionMappers;
+    private final TransactionRepository transactionRepository;
 
     public TransactionService
-            (TransactionMapper transactionMappers,
+            (TransactionServiceAudit audit,
+             TransactionMapper transactionMappers,
              TransactionRepository transactionRepository)
     {
+        this.audit = audit;
         this.transactionMappers = transactionMappers;
         this.transactionRepository = transactionRepository;
 
     }
 
-    public String createPaymentProcess(TransactionRequest request) throws JsonProcessingException {
+    public String createPaymentProcess(TransactionRequest request) {
 
-        // Create Transaction Entity
-        Transaction transaction = transactionMappers.mapToEntity(request);
-        log.info("Create Transaction", LogBuilder.serviceLog("/transaction/pix", "Payment",
-                transaction.getTransactionId().toString(), "TransactionService", "createPaymentProcess", "Payment processing starting"));
+        try {
+            // Create Transaction Entity
+            Transaction transaction = transactionMappers.mapToEntity(request);
+            UUID transactionId = transaction.getTransactionId();
 
-        transactionRepository.save(transaction);
+            audit.logCreateTransaction(transactionId.toString()); // LOG
 
-        return transaction.getTransactionId().toString();
+            transactionRepository.save(transaction);
+            return transaction.getTransactionId().toString();
+
+        } catch (DataBaseException e) {
+            audit.logErrorCreateTransaction(e.getMessage());
+            throw new DataBaseException("An error occurred while saving Transaction for request: " + request.toString());
+        }
 
     }
 }
