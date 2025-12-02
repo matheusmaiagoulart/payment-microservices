c52286a417435ad0ed385a88481e66780bd7d283
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-02 12:01:27 -0300

    feat: Add documentation for StatementService class to clarify its responsibilities

--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Application/Services/StatementService.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Application/Services/StatementService.java
@@ -9,13 +9,18 @@ import java.util.UUID;
 
 import static java.util.Arrays.stream;
 
+/**
+ * Service class is responsible for handling Statement operations.
+ * <p>
+ * It's responsible for retrieving all transaction statements for a given account ID.
+ *
+ * @author Matheus Maia Goulart
+ */
 @Service
 public class StatementService {
 
     private final TransactionRepository transactionRepository;
-    public StatementService(TransactionRepository transactionRepository) {
-        this.transactionRepository = transactionRepository;
-    }
+    public StatementService(TransactionRepository transactionRepository) { this.transactionRepository = transactionRepository; }
 
     public List<Transaction> getAllTransactionsStatements(UUID accountId){
         return transactionRepository.findTransactionsBySenderAccountIdOrReceiverAccountId(accountId, accountId)
