f2f1b80e23681a65c6f957e2ce2cc3477468788d
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-05 21:47:31 -0300

    refactor: Update WalletKeysRepository to use UUID and improve method naming

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/WalletKeysRepository.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/WalletKeysRepository.java
@@ -1,17 +1,18 @@
 package com.matheus.payments.wallet.Infra.Repository;
 
-import com.matheus.payments.wallet.Domain.Wallet.WalletKeys;
+import com.matheus.payments.wallet.Domain.WalletKeys;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface WalletKeysRepository extends JpaRepository<WalletKeys, String> {
+public interface WalletKeysRepository extends JpaRepository<WalletKeys, UUID> {
 
     @Query("SELECT wk FROM WalletKeys wk WHERE wk.keyValue = :keyValue")
-    Optional<WalletKeys> findwalletIdBykey(String keyValue);
+    Optional<WalletKeys> findAccountIdByKey(String keyValue);
 
     Boolean existsWalletKeysByKeyValue(String keyValue);
 }
