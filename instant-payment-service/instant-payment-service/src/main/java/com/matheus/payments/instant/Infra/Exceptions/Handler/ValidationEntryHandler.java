8c659b4aefbc712c147b89e5e497168d15daa623
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-16 17:20:41 -0300

    feat: Add ValidationEntryHandler for handling validation exceptions in REST API

---
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/Infra/Exceptions/Handler/ValidationEntryHandler.java
@@ -0,0 +1,27 @@
+package com.matheus.payments.instant.Infra.Exceptions.Handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationEntryHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String, String>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((org.springframework.validation.FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}
