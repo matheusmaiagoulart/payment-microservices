28717791b86dff61e19eebebcfc5812b3dfa9d00
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-16 18:29:23 -0300

    feat: Add existsByTransactionId method to TransactionProcessedRepository for transaction existence check

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/TransactionProcessedRepository.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/TransactionProcessedRepository.java
@@ -2,6 +2,9 @@ package com.matheus.payments.wallet.Infra.Repository;
 
 import com.matheus.payments.wallet.Domain.TransactionsProcessed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -9,4 +12,5 @@ import java.util.UUID;
 @Repository
 public interface TransactionProcessedRepository extends JpaRepository<TransactionsProcessed, UUID> {
 
+    boolean existsByTransactionId(UUID transactionId);
 }
