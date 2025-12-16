9a97c88d93efb3f4aea41c3246040619bcb2574d
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-16 17:16:29 -0300

    feat: Implement account debit validation with InsufficientBalanceException and InvalidAmountException

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Domain/Wallet.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Domain/Wallet.java
@@ -1,5 +1,7 @@
 package com.matheus.payments.wallet.Domain;
 
+import com.matheus.payments.wallet.Domain.Exceptions.InsufficientBalanceException;
+import com.matheus.payments.wallet.Domain.Exceptions.InvalidAmountException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -37,12 +39,25 @@ public class Wallet {
         this.createdAt = LocalDateTime.now();
     }
 
-    public void debitAccount(BigDecimal amount){
+    public void debitAccount(BigDecimal amount) {
+        
+        if(amount.compareTo(BigDecimal.ZERO) < 0) {
+            throw new InvalidAmountException();
+        }
+        if(!sufficientBalanceValidation(amount)) {
+            throw new InsufficientBalanceException();
+        }
+
         this.balance = this.balance.subtract(amount);
     }
 
-    public void creditAccount(BigDecimal amount){
+    public void creditAccount(BigDecimal amount) {
         this.balance = this.balance.add(amount);
+    }
+
+    public boolean sufficientBalanceValidation(BigDecimal amount) {
+        // If balance >= amount, return true
+        return this.balance.compareTo(amount) >= 0;
     }
 }
 
