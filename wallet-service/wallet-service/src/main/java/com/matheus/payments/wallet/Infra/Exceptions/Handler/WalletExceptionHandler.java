a028acf85175c9ece6c0d1c10256cd4013240418
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-16 17:18:43 -0300

    feat: Update WalletExceptionHandler to enhance exception handling and add new exceptions

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Exceptions/Handler/WalletExceptionHandler.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Exceptions/Handler/WalletExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.matheus.payments.wallet.Infra.Exceptions.Handler;
 
+import com.matheus.payments.wallet.Domain.Exceptions.*;
 import com.matheus.payments.wallet.Infra.Exceptions.Custom.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,32 +11,44 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class WalletExceptionHandler {
 
     @ExceptionHandler(SameUserException.class)
-    private ResponseEntity<HandlerMessage> SameUserException(SameUserException sameUserException){
+    public ResponseEntity<HandlerMessage> SameUserException(SameUserException sameUserException){
         HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.BAD_REQUEST , sameUserException.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlerMessage);
     }
 
     @ExceptionHandler(WalletNotFoundException.class)
-    private ResponseEntity<HandlerMessage> WalletNotFound(WalletNotFoundException walletNotFound){
-        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.NOT_FOUND , walletNotFound.getMessage());
+    public ResponseEntity<HandlerMessage> WalletNotFound(WalletNotFoundException walletNotFound){
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.NOT_FOUND , (walletNotFound.getMessage() + " wallet not found or inactive"));
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(handlerMessage);
     }
 
     @ExceptionHandler(InsufficientBalanceException.class)
-    private ResponseEntity<HandlerMessage> InsufficientBalanceException(InsufficientBalanceException insufficientBalanceException){
+    public ResponseEntity<HandlerMessage> InsufficientBalanceException(InsufficientBalanceException insufficientBalanceException){
         HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.BAD_REQUEST , insufficientBalanceException.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlerMessage);
     }
 
-    @ExceptionHandler(KeyValueAlreadyExists.class)
-    private ResponseEntity<HandlerMessage> KeyValueAlreadyExists(KeyValueAlreadyExists keyValueAlreadyExists){
-        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.BAD_REQUEST , keyValueAlreadyExists.getMessage());
+    @ExceptionHandler(PixKeyAlreadyRegisteredException.class)
+    public ResponseEntity<HandlerMessage> KeyValueAlreadyExists(PixKeyAlreadyRegisteredException keyValueAlreadyExists){
+    HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.BAD_REQUEST , "Key value already exists: " + keyValueAlreadyExists.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlerMessage);
     }
 
     @ExceptionHandler(TransactionAlreadyProcessed.class)
-    private ResponseEntity<HandlerMessage> TransactionAlreadyProcessed(TransactionAlreadyProcessed transactionAlreadyProcessed){
-        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.CONFLICT , "Transaction has already been processed.");
+    public ResponseEntity<HandlerMessage> TransactionAlreadyProcessed(TransactionAlreadyProcessed transactionAlreadyProcessed){
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.CONFLICT , transactionAlreadyProcessed.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(handlerMessage);
+    }
+
+    @ExceptionHandler(FailedToSaveLedgeEntry.class)
+    public ResponseEntity<HandlerMessage> FailedToSave(FailedToSaveLedgeEntry failedToSave){
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.CONFLICT , "An error occurred while creating ledger entries: " + failedToSave.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(handlerMessage);
+    }
+
+    @ExceptionHandler(InvalidAmountException.class)
+    public ResponseEntity<HandlerMessage> InvalidAmountException(InvalidAmountException invalidAmountException){
+        HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.BAD_REQUEST , invalidAmountException.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlerMessage);
     }
 }
