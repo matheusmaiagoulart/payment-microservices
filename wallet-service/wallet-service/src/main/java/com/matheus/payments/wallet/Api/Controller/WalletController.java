9fb750b38eb1886e8f0ed87583199342b32bfa9f
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-16 17:19:58 -0300

    refactor: Refactor WalletController to use use case classes for wallet creation and instant payment processing

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Api/Controller/WalletController.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Api/Controller/WalletController.java
@@ -2,8 +2,9 @@ package com.matheus.payments.wallet.Api.Controller;
 
 import com.matheus.payments.wallet.Application.DTOs.Request.CreateWalletRequest;
 import com.matheus.payments.wallet.Application.DTOs.Response.InstantPaymentResponse;
-import com.matheus.payments.wallet.Application.DTOs.Response.PaymentProcessorResponse;
-import com.matheus.payments.wallet.Application.Services.WalletService;
+import com.matheus.payments.wallet.Application.UseCases.CreateWallet;
+import com.matheus.payments.wallet.Application.UseCases.InstantPayment;
+import org.shared.DTOs.PaymentProcessorResponse;
 import org.shared.DTOs.TransactionDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +16,18 @@ import java.util.UUID;
 @RequestMapping("/wallets")
 public class WalletController {
 
-    private final WalletService walletService;
+    private final InstantPayment instantPayment;
+    private final CreateWallet createWallet;
 
-    public WalletController(WalletService walletService) {this.walletService = walletService; }
+    public WalletController(InstantPayment instantPayment, CreateWallet createWallet) {
+        this.instantPayment = instantPayment;
+        this.createWallet = createWallet;
+    }
 
     @PostMapping("/createAccount")
     public ResponseEntity<String> createWallet(@RequestBody CreateWalletRequest request) {
 
-        var result = walletService.createWallet(request);
+        var result = createWallet.createWallet(request);
 
         if (!result) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wallet already exists for this user.");
         return ResponseEntity.status(HttpStatus.CREATED).body("Wallet created successfully.");
@@ -31,12 +36,15 @@ public class WalletController {
     @PostMapping("/instant-payment")
     public PaymentProcessorResponse instantPayment(@RequestBody TransactionDTO request) {
 
-        InstantPaymentResponse result = walletService.transferProcess(request);
+        InstantPaymentResponse result = instantPayment.transferProcess(request);
 
-        if(result.isSucessful()){
-            return PaymentProcessorResponse.successResponse(UUID.fromString(request.getTransactionId()), result.getSenderAccountId(), result.getReceiverAccountId());
-        } else {
-            return PaymentProcessorResponse.failedResponse(UUID.fromString(request.getTransactionId()), result.getSenderAccountId(), result.getReceiverAccountId(), result.getFailedMessage());
+        if (!result.isSucessful()) {
+            return PaymentProcessorResponse.failedResponse(false, UUID.fromString(request.getTransactionId()), result.getSenderAccountId(), result.getReceiverAccountId(), result.getFailedMessage());
         }
+        else if (result.isAlreadyProcessed()) {
+            return PaymentProcessorResponse.failedResponse(true, UUID.fromString(request.getTransactionId()), result.getSenderAccountId(), result.getReceiverAccountId(), result.getFailedMessage());
+        }
+
+        return PaymentProcessorResponse.successResponse(UUID.fromString(request.getTransactionId()), result.getSenderAccountId(), result.getReceiverAccountId());
     }
 }
