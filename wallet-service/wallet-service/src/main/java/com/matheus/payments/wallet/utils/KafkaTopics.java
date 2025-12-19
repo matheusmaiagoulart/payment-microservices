fa63d8e6d5f0f6b5836810af8436c561ba5056e1
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-18 21:05:28 -0300

    feat: Add INSTANT_PAYMENT_TOPIC_RESPONSE constant for handling payment responses

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/utils/KafkaTopics.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/utils/KafkaTopics.java
@@ -4,6 +4,7 @@ package com.matheus.payments.wallet.utils;
 public class KafkaTopics {
 
     public static final String INSTANT_PAYMENT_TOPIC = "instant-payments";
+    public static final String INSTANT_PAYMENT_TOPIC_RESPONSE = "instant-payments-response";
 
 
 }
