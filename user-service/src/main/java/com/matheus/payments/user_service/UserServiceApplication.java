4f97114393acbdc27020fd2193f3d7e46e06444b
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-23 19:58:37 -0300

    feat: Initialize user service with application properties, POM, and test setup

---
+++ user-service/src/main/java/com/matheus/payments/user_service/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.matheus.payments.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@SpringBootApplication
+@EnableKafka
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
