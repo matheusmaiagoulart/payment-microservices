7f4c51e32bedb0426a75ac31383a11d9b7210ab2
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2025-12-23 19:58:37 -0300

    feat: Initialize user service with application properties, POM, and test setup

--- instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/InstantPaymentServiceApplication.java
+++ instant-payment-service/instant-payment-service/src/main/java/com/matheus/payments/instant/InstantPaymentServiceApplication.java
@@ -1,10 +1,14 @@
 package com.matheus.payments.instant;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 
-
+@Configuration
+@EnableRetry
 @SpringBootApplication
 public class InstantPaymentServiceApplication {
 
---
+++ user-service/src/test/java/com/matheus/payments/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.payments.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
