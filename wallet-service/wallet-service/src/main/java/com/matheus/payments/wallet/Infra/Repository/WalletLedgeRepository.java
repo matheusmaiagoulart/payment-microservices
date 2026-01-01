22800883f064ee444d3a55defcb8a97ea71717dd
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-01 14:59:38 -0300

    refactor: Update import statements to use Models package for WalletLedger and Wallet

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/WalletLedgeRepository.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/WalletLedgeRepository.java
@@ -1,6 +1,6 @@
 package com.matheus.payments.wallet.Infra.Repository;
 
-import com.matheus.payments.wallet.Domain.WalletLedger;
+import com.matheus.payments.wallet.Domain.Models.WalletLedger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
