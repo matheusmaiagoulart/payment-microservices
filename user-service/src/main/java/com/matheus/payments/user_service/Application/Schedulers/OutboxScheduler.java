3163ac03cc27921dc11de81b83638df22f01793c
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-01 21:09:35 -0300

    feat: Add OutboxScheduler for processing pending outbox events

---
+++ user-service/src/main/java/com/matheus/payments/user_service/Application/Schedulers/OutboxScheduler.java
@@ -0,0 +1,51 @@
+package com.matheus.payments.user_service.Application.Schedulers;
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tools.jackson.databind.ObjectMapper;
+import com.matheus.payments.user_service.Application.Interfaces.UserEventPublisher;
+import com.matheus.payments.user_service.Domain.Events.UserCreatedEvent;
+import com.matheus.payments.user_service.Domain.Models.Outbox;
+import com.matheus.payments.user_service.Infra.Repository.OutboxRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OutboxScheduler {
+
+    private final OutboxRepository outboxRepository;
+    private final UserEventPublisher publisher;
+    private final ObjectMapper objectMapper;
+
+    public OutboxScheduler(ObjectMapper mapper, OutboxRepository outboxRepository, UserEventPublisher publisher) {
+        this.outboxRepository = outboxRepository;
+        this.publisher = publisher;
+        this.objectMapper = mapper;
+    }
+
+    @Scheduled(fixedDelay = 6000)
+    public void sendToCreateWallet() {
+
+        List<Outbox> pendingToSend;
+
+        pendingToSend = outboxRepository.findAllBySentFalse();
+        pendingToSend.forEach(outbox -> sendOutboxEvent(outbox));
+    }
+
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW) // Always create a new transaction for each outbox event
+    public void sendOutboxEvent(Outbox outbox) {
+        try {
+            UserCreatedEvent userCreatedEvent = objectMapper.readValue(outbox.getPayload(), UserCreatedEvent.class);
+            publisher.publisher(userCreatedEvent);
+            outbox.setSent(true);
+            outboxRepository.save(outbox);
+        } catch (Exception e) {
+            outbox.setFailed(true);
+            outbox.setFailureReason(e.getMessage());
+            outboxRepository.save(outbox);
+        }
+    }
+}
