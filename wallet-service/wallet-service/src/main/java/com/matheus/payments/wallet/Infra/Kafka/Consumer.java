b4bc27228a975919a91cf1b791a42385ec18980b
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-01 21:14:08 -0300

    feat: Enhance Kafka consumer configuration with manual ACK support

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Kafka/Consumer.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Kafka/Consumer.java
@@ -3,8 +3,10 @@ package com.matheus.payments.wallet.Infra.Kafka;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,13 +15,30 @@ import java.util.Map;
 public class Consumer {
 
     @Bean
-    public ConsumerFactory<String, String> consumerFactory()
-    {
+    public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "wallet-service-group");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+
+        configProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
+
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory());
+
+        // Configura o ACK manual após processamento bem-sucedido
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setConsumerFactory(consumerFactory());
+
+        return factory;
+    }
+
 }
