dce4683c2afcca7634217d884ee8cd0476cfda54
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-01 21:16:35 -0300

    feat: Refactor import statement for TransactionsProcessed model in TransactionProcessedRepository

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/TransactionProcessedRepository.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/TransactionProcessedRepository.java
@@ -1,10 +1,7 @@
 package com.matheus.payments.wallet.Infra.Repository;
 
-import com.matheus.payments.wallet.Domain.TransactionsProcessed;
+import com.matheus.payments.wallet.Domain.Models.TransactionsProcessed;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
