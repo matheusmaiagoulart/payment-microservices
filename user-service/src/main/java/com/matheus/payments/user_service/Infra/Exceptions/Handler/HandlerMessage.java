7b17d046b2f29c9f16e044aec46c0da009efd93d
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-01 21:07:58 -0300

    feat: Add HandlerMessage class for standardized exception handling responses

---
+++ user-service/src/main/java/com/matheus/payments/user_service/Infra/Exceptions/Handler/HandlerMessage.java
@@ -0,0 +1,21 @@
+package com.matheus.payments.user_service.Infra.Exceptions.Handler;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class HandlerMessage {
+    private HttpStatus status;
+    private String message;
+    private LocalDateTime timestamp;
+
+    public HandlerMessage(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
+}
