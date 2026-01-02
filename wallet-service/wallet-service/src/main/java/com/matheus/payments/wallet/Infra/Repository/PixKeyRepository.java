e1ed26b0dab1e7754338975b81fce502845d3098
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-02 17:17:52 -0300

    feat: Update import statement for PixKey model to reflect new package structure

--- wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/PixKeyRepository.java
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Repository/PixKeyRepository.java
@@ -1,6 +1,6 @@
 package com.matheus.payments.wallet.Infra.Repository;
 
-import com.matheus.payments.wallet.Domain.PixKey;
+import com.matheus.payments.wallet.Domain.Models.PixKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
