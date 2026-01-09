b270d7cc41f86c8a59e0b32cfff05c78f46d935a
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-09 20:38:01 -0300

    feat: Add Kafka producer configuration for wallet events

---
+++ wallet-service/wallet-service/src/main/java/com/matheus/payments/wallet/Infra/Kafka/Producer.java
@@ -0,0 +1,62 @@
+package com.matheus.payments.wallet.Infra.Kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class Producer {
+
+    private final KafkaProperties kafkaProperties;
+
+    public Producer(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory()
+    {
+        Map<String, Object> properties = kafkaProperties .buildProducerProperties();
+
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.RETRIES_CONFIG, 3); // número de tentativas em caso de falha
+        properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true); // garante que a mensagem seja entregue apenas uma vez
+        properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1); // uma mensagem por vez
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    public NewTopic WalletCreated() {
+        return TopicBuilder
+                .name("WalletCreated")
+                .partitions(1) //numero de consumidores que podem ler o topico em paralelo
+                .replicas(1)
+                .config("retention.ms", "604800000") //7 dias em milisegundos
+                .build();
+    }
+
+    public NewTopic WalletCreationFailed() {
+        return TopicBuilder
+                .name("WalletCreationFailed")
+                .partitions(1) //numero de consumidores que podem ler o topico em paralelo
+                .replicas(1)
+                .config("retention.ms", "604800000") //7 dias em milisegundos
+                .build();
+    }
+}
