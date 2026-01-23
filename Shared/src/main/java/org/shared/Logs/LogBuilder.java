88133c76c3941071fa45059a7db7cedae94ce7f6
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-23 17:11:51 -0300

    refactor: Simplify logging methods in LogBuilder and update version in pom.xml

--- Shared/src/main/java/org/shared/Logs/LogBuilder.java
+++ Shared/src/main/java/org/shared/Logs/LogBuilder.java
@@ -1,49 +1,42 @@
 package org.shared.Logs;
 
 
+import java.util.List;
+
 import static net.logstash.logback.argument.StructuredArguments.kv;
 
 public class LogBuilder {
 
-    public static Object[] serviceLog(String requestURL, String MS, String transactionId, String className, String methodName, String message) {
-        return new Object[]{
-                kv("source_endpoint", requestURL),
+    public static List<Object> baseLog(
+            String MS,
+            String correlationId,
+            String className,
+            String methodName,
+            String message) {
+        return List.of(
                 kv("service_name", MS),
-                kv("transactionId", transactionId),
+                kv("correlation_id", correlationId),
                 kv("class", className),
                 kv("method", methodName),
                 kv("event", message)
-        };
+        );
     }
 
-    public static Object[] eventLog(String event, String topic, String MS, String keyValue, String className, String methodName, String message) {
-        return new Object[]{
+    public static List<Object> eventLog
+            (String event,
+             String topic,
+             String keyValue) {
+        return List.of(
                 kv("event_type", event),
                 kv("topic_listened", topic),
-                kv("key_value", keyValue),
-                kv("service_name", MS),
-                kv("class", className),
-                kv("method", methodName),
-                kv("event", message)
-        };
+                kv("key_value", keyValue));
     }
 
-    public static Object[] requestLog(String httpMethod, String endpoint, String MS, String transactionId, String className, String methodName, Object... additionalKVs) {
-        var objBase = new Object[]{
+    public static List<Object> requestLog
+            (String httpMethod,
+            String endpoint) {
+        return List.of(
                 kv("http_method", httpMethod),
-                kv("target_endpoint", endpoint),
-                kv("service_name", MS),
-                kv("transactionId", transactionId),
-                kv("class", className),
-                kv("method", methodName)
-        };
-
-        Object[] log = new Object[objBase.length + additionalKVs.length];
-
-        // obj base, index start copy, destination, index start array final, length array base
-        System.arraycopy(objBase, 0, log, 0, objBase.length);
-        System.arraycopy(additionalKVs, 0, log, objBase.length, additionalKVs.length);
-
-        return log;
+                kv("target_endpoint", endpoint));
     }
 }
