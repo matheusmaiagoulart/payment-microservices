70edb1b13c4b0142d1323965735712061ec39214
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-24 14:44:49 -0300

    feat: Enable Kafka and Retry support in UserServiceApplication

--- MS-User/src/main/java/com/matheus/payments/user_service/UserServiceApplication.java
+++ MS-User/src/main/java/com/matheus/payments/user_service/UserServiceApplication.java
@@ -1,13 +1,15 @@
 package com.matheus.payments.user_service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableKafka
+@EnableRetry
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
