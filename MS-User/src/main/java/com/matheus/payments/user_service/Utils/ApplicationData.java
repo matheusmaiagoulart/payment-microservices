32538d4bbc3ac1f7b007f7183b1aae680b9c85d2
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-24 14:26:14 -0300

    feat: Add ApplicationData and KafkaTopics classes for application configuration

---
+++ MS-User/src/main/java/com/matheus/payments/user_service/Utils/ApplicationData.java
@@ -0,0 +1,7 @@
+package com.matheus.payments.user_service.Utils;
+
+public class ApplicationData {
+
+    public static final String APPLICATION_NAME = "MS-User";
+    public static final String APPLICATION_CONSUMER_NAME = "user-service-group";
+}
---
+++ MS-User/src/main/java/com/matheus/payments/user_service/Utils/KafkaTopics.java
@@ -0,0 +1,7 @@
+package com.matheus.payments.user_service.Utils;
+
+public class KafkaTopics {
+
+    public static final String USER_CREATED = "user-created";
+    public static final String WALLET_CREATED_TOPIC = "wallet-created";
+}
