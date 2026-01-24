2a8984335c9e0b91dc57cfe07419887afc14d82e
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-24 14:39:56 -0300

    refactor: Remove unused classes

--- MS-User/src/main/java/com/matheus/payments/user_service/Application/Interfaces/UserEventPublisher.java
+++
@@ -1,8 +0,0 @@
-package com.matheus.payments.user_service.Application.Interfaces;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.matheus.payments.user_service.Domain.Events.UserCreatedEvent;
-
-public interface UserEventPublisher {
-    void publisher(UserCreatedEvent event) throws JsonProcessingException;
-}
