8ca8469ab1a997432ab98f873d028bcc08d61fde
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-24 14:32:53 -0300

    feat: Refactor OutboxScheduler to use KafkaTemplate for event publishing and update package structure

--- MS-User/src/main/java/com/matheus/payments/user_service/Infra/Schedulers/OutboxScheduler.java
+++ MS-User/src/main/java/com/matheus/payments/user_service/Infra/Schedulers/OutboxScheduler.java
@@ -1,10 +1,11 @@
-package com.matheus.payments.user_service.Application.Schedulers;
+package com.matheus.payments.user_service.Infra.Schedulers;
 
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import tools.jackson.databind.ObjectMapper;
-import com.matheus.payments.user_service.Application.Interfaces.UserEventPublisher;
-import com.matheus.payments.user_service.Domain.Events.UserCreatedEvent;
 import com.matheus.payments.user_service.Domain.Models.Outbox;
 import com.matheus.payments.user_service.Infra.Repository.OutboxRepository;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,34 +13,39 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * This scheduler is responsible for sending pending outbox events to Kafka.
+ * It runs at a fixed delay and processes all outbox events that have not been sent yet.
+ *
+ * @author Matheus Maia Goulart
+ */
 @Service
 public class OutboxScheduler {
 
     private final OutboxRepository outboxRepository;
-    private final UserEventPublisher publisher;
-    private final ObjectMapper objectMapper;
+    private final KafkaTemplate<String, String> publisher;
 
-    public OutboxScheduler(ObjectMapper mapper, OutboxRepository outboxRepository, UserEventPublisher publisher) {
+    public OutboxScheduler(OutboxRepository outboxRepository, KafkaTemplate<String, String> publisher) {
         this.outboxRepository = outboxRepository;
         this.publisher = publisher;
-        this.objectMapper = mapper;
     }
 
     @Scheduled(fixedDelay = 6000)
     public void sendToCreateWallet() {
-
-        List<Outbox> pendingToSend;
-
-        pendingToSend = outboxRepository.findAllBySentFalse();
+        List<Outbox> pendingToSend = outboxRepository.findAllBySentFalse();
         pendingToSend.forEach(outbox -> sendOutboxEvent(outbox));
     }
-
 
     @Transactional(propagation = Propagation.REQUIRES_NEW) // Always create a new transaction for each outbox event
     public void sendOutboxEvent(Outbox outbox) {
         try {
-            UserCreatedEvent userCreatedEvent = objectMapper.readValue(outbox.getPayload(), UserCreatedEvent.class);
-            publisher.publisher(userCreatedEvent);
+            Message<String> message = MessageBuilder
+                    .withPayload(outbox.getPayload())
+                    .setHeader("correlationId", outbox.getCorrelationId().toString())
+                    .setHeader(KafkaHeaders.TOPIC, outbox.getTopic())
+                    .build();
+
+            publisher.send(message);
             setOutboxSent(outbox);
         } catch (Exception e) {
             setOutboxFailed(outbox, e.getMessage());
