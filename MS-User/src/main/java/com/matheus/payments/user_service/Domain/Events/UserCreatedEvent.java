469038742d8853e312ecd2356494f7e151f3710d
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-24 14:39:01 -0300

    feat: Update UserCreatedEvent to include userId and timestamp

--- MS-User/src/main/java/com/matheus/payments/user_service/Domain/Events/UserCreatedEvent.java
+++ MS-User/src/main/java/com/matheus/payments/user_service/Domain/Events/UserCreatedEvent.java
@@ -5,22 +5,24 @@ import lombok.NoArgsConstructor;
 import org.shared.Domain.accountType;
 import org.shared.Domain.keyType;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @NoArgsConstructor
 @Getter
 public class UserCreatedEvent {
 
-    private UUID id;
+    private UUID userId;
     private String cpf;
     private keyType type;
     private accountType accountType;
-    private final String topic = "UserCreated";
+    private LocalDateTime timestamp;
 
-    public UserCreatedEvent(UUID id, String cpf, accountType accountType) {
-        this.id = id;
+    public UserCreatedEvent(UUID userId, String cpf, accountType accountType) {
+        this.userId = userId;
         this.cpf = cpf;
         this.type = keyType.CPF; // Default PIX key type is CPF when user is created
         this.accountType = accountType;
+        this.timestamp = LocalDateTime.now();
     }
 }
