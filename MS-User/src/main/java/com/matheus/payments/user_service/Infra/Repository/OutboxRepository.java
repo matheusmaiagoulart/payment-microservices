c27234e97cfdb33c85a14f9762b3c5b629284c2a
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-24 14:29:55 -0300

    feat: Update OutboxRepository to use native query and add Repository annotation

--- MS-User/src/main/java/com/matheus/payments/user_service/Infra/Repository/OutboxRepository.java
+++ MS-User/src/main/java/com/matheus/payments/user_service/Infra/Repository/OutboxRepository.java
@@ -3,13 +3,14 @@ package com.matheus.payments.user_service.Infra.Repository;
 import com.matheus.payments.user_service.Domain.Models.Outbox;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface OutboxRepository extends JpaRepository<Outbox, UUID> {
 
-    @Query(value = "SELECT o FROM Outbox o WHERE o.isSent = false LIMIT 100")
+    @Query(value = "SELECT TOP 100 * FROM outbox WHERE is_sent = 0", nativeQuery = true)
     List<Outbox> findAllBySentFalse();
 }
