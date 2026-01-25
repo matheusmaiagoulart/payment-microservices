fe224871ba7ca2fdb84a812fd8c27f98b3f3edb7
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-25 15:20:56 -0300

    feat: Add custom Jackson configuration for JSON serialization

---
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/Infra/Configs/JacksonConfig.java
@@ -0,0 +1,20 @@
+package com.matheus.payments.wallet.Infra.Configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+    }
+}
