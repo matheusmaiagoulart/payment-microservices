ac38f6da7d5a77a68cf96d548f42243697442c9d
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-25 15:19:32 -0300

    feat: Introduce ApplicationData utility class

---
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/utils/ApplicationData.java
@@ -0,0 +1,8 @@
+package com.matheus.payments.wallet.utils;
+
+public class ApplicationData {
+
+    public static final String APPLICATION_NAME = "MS-Wallet";
+
+    public static final String APPLICATION_CONSUMER_GROUP = "wallet-service-group";
+}
