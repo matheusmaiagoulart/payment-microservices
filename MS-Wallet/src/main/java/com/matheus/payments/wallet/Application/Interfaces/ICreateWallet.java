7aa5addfb51f906bca259025b3a1a2d4e569e51b
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-25 15:38:06 -0300

    refactor: remove unused interface

--- MS-Wallet/src/main/java/com/matheus/payments/wallet/Application/Interfaces/ICreateWallet.java
+++
@@ -1,7 +0,0 @@
-package com.matheus.payments.wallet.Application.Interfaces;
-
-import com.matheus.payments.wallet.Domain.Events.UserCreatedEvent;
-
-public interface ICreateWallet {
-    boolean createWallet(UserCreatedEvent request);
-}
