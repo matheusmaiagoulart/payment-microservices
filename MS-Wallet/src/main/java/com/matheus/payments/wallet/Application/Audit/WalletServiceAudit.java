dbf3d11c4b40c11f41fedee866128d81333080d5
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-25 15:28:31 -0300

    refactor: Enhance logging in UserCreatedListenerAudit and WalletServiceAudit for better event tracking

--- MS-Wallet/src/main/java/com/matheus/payments/wallet/Application/Audit/WalletServiceAudit.java
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/Application/Audit/WalletServiceAudit.java
@@ -1,8 +1,12 @@
 package com.matheus.payments.wallet.Application.Audit;
 
+import com.matheus.payments.wallet.utils.ApplicationData;
+import com.matheus.payments.wallet.utils.KafkaTopics;
 import lombok.extern.slf4j.Slf4j;
 import org.shared.Logs.LogBuilder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 import static net.logstash.logback.argument.StructuredArguments.kv;
 
@@ -12,61 +16,87 @@ public class WalletServiceAudit {
 
     private final String CLASS_NAME = "WalletService";
     private final String METHOD_NAME = "transferProcess";
-    private final String MICROSERVICE_NAME = "wallet-service";
+    private final String WALLET_ENDPOINT = "/wallets/instant-payment";
 
-
+    // Transfer Process Logs
     public void logStartingTransferProcess(String transactionId) {
-        log.info("Starting transfer process",
-                LogBuilder.serviceLog("/wallets/instant-payment", MICROSERVICE_NAME, transactionId, CLASS_NAME, METHOD_NAME,
-                        "transfer.process.starting"));
+        ArrayList<Object> logData = new ArrayList<>();
+
+        logData.addAll(LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME, "Starting transfer process"));
+        logData.addAll(LogBuilder.requestLog("POST", "/wallets/instant-payment"));
+        logData.add(kv("transactionId", transactionId));
+
+        log.info("Starting transfer process", logData.toArray());
     }
 
     public void logBalanceValidation(String transactionId) {
-        log.info("Validating balance",
-                LogBuilder.serviceLog("/wallets/instant-payment", MICROSERVICE_NAME, transactionId, CLASS_NAME, METHOD_NAME,
-                        "transfer.process.balance.validation"));
+        ArrayList<Object> logData = new ArrayList<>();
+
+        logData.addAll(LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME, "Validating balance operation"));
+        logData.add(kv("transactionId", transactionId));
+
+        log.info("Validating balance", logData.toArray());
     }
 
     public void logTransferSuccess(String transactionId) {
-        log.info("Transfer completed successfully",
-                LogBuilder.serviceLog("/wallets/instant-payment", MICROSERVICE_NAME, transactionId, CLASS_NAME, METHOD_NAME,
-                        "transfer.process.success"));
+        ArrayList<Object> logData = new ArrayList<>();
+
+        logData.addAll(LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME, "Transfer completed successfully"));
+        logData.add(kv("transactionId", transactionId));
+        logData.add(kv("event", "transfer.process.success"));
+
+        log.info("Transfer completed successfully", logData.toArray());
     }
 
     public void logTransferError(String transactionId, String errorMessage) {
-        log.info("Transfer failed",
-                LogBuilder.serviceLog("/wallets/instant-payment", MICROSERVICE_NAME, transactionId, CLASS_NAME, METHOD_NAME,
-                        "transfer.process.failed"),
-                kv("errorMessage", errorMessage));
-    }
+        ArrayList<Object> logData = new ArrayList<>();
 
-    public void logAlreadyProcessed(String transactionId) {
-        log.info("Transfer already processed by Transaction ID",
-                LogBuilder.serviceLog("/wallets/instant-payment", MICROSERVICE_NAME, transactionId, CLASS_NAME, METHOD_NAME,
-                        "transfer.process.failed"));
+        logData.addAll(LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME, "Transfer failed"));
+        logData.add(kv("transactionId", transactionId));
+        logData.add(kv("errorMessage", errorMessage));
+        logData.add(kv("event", "transfer.process.failed"));
+
+        log.error("Transfer failed", logData.toArray());
     }
 
 
     // Create Wallet Logs
 
     public void logCreatingWallet(String keyValue) {
-        log.info("Creating wallet for user: " + keyValue,
-                LogBuilder.serviceLog("/wallets/instant-payment", MICROSERVICE_NAME, null, CLASS_NAME, METHOD_NAME,
-                        "create.wallet.process.start"));
+        ArrayList<Object> logData = new ArrayList<>();
+
+        logData.addAll(LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME, "Creating wallet for user: " + keyValue));
+        logData.addAll(LogBuilder.requestLog("POST", WALLET_ENDPOINT));
+        logData.addAll(LogBuilder.eventLog("UserCreated", KafkaTopics.USER_CREATED_EVENT_TOPIC, keyValue));
+        logData.add(kv("keyValue", keyValue));
+        logData.add(kv("event", "create.wallet.process.start"));
+
+        log.info("Creating wallet for user: " + keyValue, logData.toArray());
     }
 
     public void logFailedCreateWallet(String keyValue) {
-        log.info("The key value already exists: " + keyValue,
-                LogBuilder.serviceLog("/wallets/instant-payment", MICROSERVICE_NAME, null, CLASS_NAME, METHOD_NAME,
-                        "create.wallet.process.failed"));
+        ArrayList<Object> logData = new ArrayList<>();
+
+        logData.addAll(LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME, "The key value already exists: " + keyValue));
+        logData.addAll(LogBuilder.requestLog("POST", WALLET_ENDPOINT));
+        logData.addAll(LogBuilder.eventLog("UserCreated", KafkaTopics.USER_CREATED_EVENT_TOPIC, keyValue));
+        logData.add(kv("keyValue", keyValue));
+        logData.add(kv("event", "create.wallet.process.failed"));
+
+        log.error("The key value already exists: " + keyValue, logData.toArray());
     }
 
     public void logFailedGeneric(String keyValue, String message) {
-        log.info(message + keyValue,
-                LogBuilder.serviceLog("/wallets/instant-payment", MICROSERVICE_NAME, null, CLASS_NAME, METHOD_NAME,
-                        "create.wallet.process.failed"));
-    }
+        ArrayList<Object> logData = new ArrayList<>();
 
+        logData.addAll( LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME, message + keyValue));
+        logData.addAll(LogBuilder.requestLog("POST", WALLET_ENDPOINT));
+        logData.addAll(LogBuilder.eventLog("UserCreated", KafkaTopics.USER_CREATED_EVENT_TOPIC, keyValue));
+        logData.add(kv("keyValue", keyValue));
+        logData.add(kv("event", "create.wallet.process.failed"));
+
+        log.warn(message + keyValue, logData.toArray());
+    }
 
 
 }
--- MS-Wallet/src/main/java/com/matheus/payments/wallet/Infra/Audit/UserCreatedListenerAudit.java
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/Infra/Audit/UserCreatedListenerAudit.java
@@ -1,8 +1,14 @@
 package com.matheus.payments.wallet.Infra.Audit;
 
+import com.matheus.payments.wallet.Application.Audit.CorrelationId;
+import com.matheus.payments.wallet.utils.ApplicationData;
 import lombok.extern.slf4j.Slf4j;
 import org.shared.Logs.LogBuilder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 @Slf4j
 @Component
@@ -10,26 +16,40 @@ public class UserCreatedListenerAudit {
 
     private final String CLASS_NAME = "UserCreatedListener";
     private final String METHOD_NAME = "createUserWalletListener";
-    private final String MICROSERVICE_NAME = "wallet-service";
     private final String TOPIC_NAME = "UserCreated";
     private final String EVENT_NAME = "UserCreated";
 
     public void logUserCreatedEventReceived(String keyValue) {
-        log.info("Received UserCreated event for key: " + keyValue,
-                LogBuilder.eventLog(EVENT_NAME, TOPIC_NAME, MICROSERVICE_NAME, keyValue, CLASS_NAME, METHOD_NAME,
-                        "user_created.event.received"));
+        ArrayList<Object> logData = new ArrayList<>();
+
+        logData.addAll(LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME, "Received UserCreated event for key:" + keyValue));
+        logData.addAll(LogBuilder.eventLog(EVENT_NAME, TOPIC_NAME, keyValue));
+        logData.add(kv("event", "user_created.event.received"));
+
+        log.info("Received UserCreated event for key: " + keyValue, logData.toArray());
     }
 
     public void logMessageProcessed(String keyValue) {
-        log.info("Successfully processed UserCreated event for key: " + keyValue,
-                LogBuilder.eventLog(EVENT_NAME, TOPIC_NAME, MICROSERVICE_NAME, keyValue, CLASS_NAME, METHOD_NAME,
-                        "user_created.event.processed"));
+        ArrayList<Object> logData = new ArrayList<>();
+
+        logData.addAll(LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME,
+                "Successfully processed UserCreated event for key:" + keyValue));
+        logData.addAll(LogBuilder.eventLog(EVENT_NAME, TOPIC_NAME, keyValue));
+        logData.add(kv("event", "user_created.event.processed"));
+
+        log.info("Successfully processed UserCreated event for key: " + keyValue, logData.toArray());
     }
 
-    public void logFailedToProcessMessage(String keyValue , String errorMessage) {
-        log.error("Failed to process UserCreated event for key: " + keyValue,
-                LogBuilder.eventLog(EVENT_NAME, TOPIC_NAME, MICROSERVICE_NAME, keyValue, CLASS_NAME, METHOD_NAME,
-                        "user_created.event.processing.failed"), errorMessage);
+    public void logFailedToProcessMessage(String keyValue, String errorMessage) {
+        ArrayList<Object> logData = new ArrayList<>();
+
+        logData.addAll(LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), CLASS_NAME, METHOD_NAME,
+                "Failed to process UserCreated event for key:" + keyValue));
+        logData.addAll(LogBuilder.eventLog(EVENT_NAME, TOPIC_NAME, keyValue));
+        logData.add(kv("event", "user_created.event.processing.failed"));
+        logData.add(kv("errorMessage", errorMessage));
+
+        log.error("Failed to process UserCreated event for key: " + keyValue, logData.toArray());
     }
 
 }
