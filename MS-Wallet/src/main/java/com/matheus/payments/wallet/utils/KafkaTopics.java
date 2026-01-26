8f25fa1e99954322fe4c190d186e17a4e938ad6c
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-01-26 18:04:08 -0300

    refactor: Remove unnecessary blank line in KafkaTopics.java

--- MS-Wallet/src/main/java/com/matheus/payments/wallet/utils/KafkaTopics.java
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/utils/KafkaTopics.java
@@ -5,7 +5,6 @@ public class KafkaTopics {
 
     public static final String WALLET_CREATED_EVENT_TOPIC = "wallet-created";
     public static final String WALLET_CREATION_FAILED_TOPIC = "wallet-creation-failed";
-
     public static final String USER_CREATED_EVENT_TOPIC = "user-created";
 
 
