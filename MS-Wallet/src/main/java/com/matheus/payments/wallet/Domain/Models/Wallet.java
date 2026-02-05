a220a6c9af012a9f5df83758b5ae48e21c7cd136
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-02-05 11:56:46 -0300

    feat: Normalize amount handling in Wallet class for debit and credit operations

--- MS-Wallet/src/main/java/com/matheus/payments/wallet/Domain/Models/Wallet.java
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/Domain/Models/Wallet.java
@@ -7,17 +7,18 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.internal.constraintvalidators.hv.br.CPFValidator;
 import org.shared.Domain.accountType;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "wallets")
 @NoArgsConstructor
-@Getter @Setter
+@Getter
+@Setter
 public class Wallet {
 
     @Id
@@ -49,19 +50,21 @@ public class Wallet {
     }
 
     public void debitAccount(BigDecimal amount) {
-        
-        if(amount.compareTo(BigDecimal.ZERO) < 0) {
+
+        BigDecimal normalizedAmount = amount.setScale(2, RoundingMode.HALF_UP); // Ensure amount has 2 decimal places, and round up if necessary
+
+        if (normalizedAmount.compareTo(BigDecimal.ZERO) < 0) {
             throw new InvalidAmountException();
         }
-        if(!sufficientBalanceValidation(amount)) {
+        if (!sufficientBalanceValidation(normalizedAmount)) {
             throw new InsufficientBalanceException();
         }
-
-        this.balance = this.balance.subtract(amount);
+        this.balance = this.balance.subtract(normalizedAmount).setScale(2, RoundingMode.HALF_UP);
     }
 
     public void creditAccount(BigDecimal amount) {
-        this.balance = this.balance.add(amount);
+        BigDecimal normalizedAmount = amount.setScale(2, RoundingMode.HALF_UP); // Ensure amount has 2 decimal places, and round up if necessary
+        this.balance = this.balance.add(normalizedAmount).setScale(2, RoundingMode.HALF_UP);
     }
 
     public boolean sufficientBalanceValidation(BigDecimal amount) {
