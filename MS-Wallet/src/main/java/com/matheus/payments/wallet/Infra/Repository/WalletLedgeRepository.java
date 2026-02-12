4413e88684dc61511d7a599ce4b897f5c316cfa0
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-02-12 16:28:34 -0300

    feat: Add method to find WalletLedger by transaction ID

--- MS-Wallet/src/main/java/com/matheus/payments/wallet/Infra/Repository/WalletLedgeRepository.java
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/Infra/Repository/WalletLedgeRepository.java
@@ -3,7 +3,10 @@ package com.matheus.payments.wallet.Infra.Repository;
 import com.matheus.payments.wallet.Domain.Models.WalletLedger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface WalletLedgeRepository extends JpaRepository<WalletLedger, UUID> {
+
+    List<WalletLedger> findByTransactionId(UUID transactionId);
 }
