004aed6b49b1c9d31d56c28ac29d6b413471f367
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-02-12 16:34:42 -0300

    feat: Enable retry mechanism in WalletServiceApplication

--- MS-Wallet/src/main/java/com/matheus/payments/wallet/WalletServiceApplication.java
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/WalletServiceApplication.java
@@ -3,10 +3,12 @@ package com.matheus.payments.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAutoConfiguration
 @EnableScheduling
+@EnableRetry
 @SpringBootApplication
 public class WalletServiceApplication {
 
