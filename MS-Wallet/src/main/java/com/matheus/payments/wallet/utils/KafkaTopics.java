eb2e587fff3864ca152f803f67f3b1f957b6ac36
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-01 10:09:31 -0300

    feat: Add deposit-related topics to KafkaTopics

--- MS-Wallet/src/main/java/com/matheus/payments/wallet/utils/KafkaTopics.java
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/utils/KafkaTopics.java
@@ -7,5 +7,6 @@ public class KafkaTopics {
     public static final String WALLET_CREATION_FAILED_TOPIC = "wallet-creation-failed";
     public static final String USER_CREATED_EVENT_TOPIC = "user-created";
 
-
+    public static final String DEPOSIT_TOPIC = "deposit-created";
+    public static final String DEPOSIT_EXECUTED_TOPIC = "deposit-executed";
 }
