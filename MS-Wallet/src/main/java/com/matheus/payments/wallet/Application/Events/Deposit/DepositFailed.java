2c46a137f97e961c8ff24e53ec0a9a483b015fce
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-02 17:39:50 -0300

    feat: Introduce DepositCreated and update DepositFailed classes to include depositId for better tracking

--- MS-Wallet/src/main/java/com/matheus/payments/wallet/Application/Events/Deposit/DepositFailed.java
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/Application/Events/Deposit/DepositFailed.java
@@ -9,13 +9,15 @@ import java.util.UUID;
 @Getter
 public class DepositFailed {
 
+    private UUID depositId;
     private UUID receiverId;
     private BigDecimal amount;
     private boolean successful;
     private String failureReason;
     private LocalDateTime timestamp;
 
-    public DepositFailed(boolean successful, UUID userId, BigDecimal amount, String failureReason) {
+    public DepositFailed(UUID depositId, boolean successful, UUID userId, BigDecimal amount, String failureReason) {
+        this.depositId = depositId;
         this.receiverId = userId;
         this.amount = amount;
         this.successful = successful;
---
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/Infra/Kafka/Listeners/DepositCreated/DepositCreated.java
@@ -0,0 +1,21 @@
+package com.matheus.payments.wallet.Infra.Kafka.Listeners.DepositCreated;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepositCreated {
+
+    private UUID depositId;
+    private UUID senderId;
+    private UUID receiverId;
+    private BigDecimal amount;
+}
