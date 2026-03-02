6ecb698b16ac1b83361675301445070b413d5a8a
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-02 17:45:59 -0300

    feat: Add DEPOSIT_FAILED_TOPIC constant for tracking failed deposit events

--- MS-Wallet/src/main/java/com/matheus/payments/wallet/utils/KafkaTopics.java
+++ MS-Wallet/src/main/java/com/matheus/payments/wallet/utils/KafkaTopics.java
@@ -9,4 +9,5 @@ public class KafkaTopics {
 
     public static final String DEPOSIT_TOPIC = "deposit-created";
     public static final String DEPOSIT_EXECUTED_TOPIC = "deposit-executed";
+    public static final String DEPOSIT_FAILED_TOPIC = "deposit-failed";
 }
