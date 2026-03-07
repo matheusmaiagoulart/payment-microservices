d8313be8e1c13469b8a2e9f54f78fd0cf1e333ed
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-07 12:25:57 -0300

    feat: Add KafkaTopics class for managing deposit-related Kafka topics

---
+++ MS-Payments/src/main/java/com/matheus/payments/Utils/KafkaTopics.java
@@ -0,0 +1,8 @@
+package com.matheus.payments.Utils;
+
+public class KafkaTopics {
+
+    public static final String DEPOSIT_TOPIC = "deposit-created";
+    public static final String DEPOSIT_EXECUTED_TOPIC = "deposit-executed";
+    public static final String DEPOSIT_FAILED_TOPIC = "deposit-failed";
+}
