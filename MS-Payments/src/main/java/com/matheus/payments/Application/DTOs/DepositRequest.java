0667f2f042feec94e37e1b890e2eee886b670189
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-07 12:23:16 -0300

    feat: Add DepositRequest DTO for handling deposit transaction requests

---
+++ MS-Payments/src/main/java/com/matheus/payments/Application/DTOs/DepositRequest.java
@@ -0,0 +1,24 @@
+package com.matheus.payments.Application.DTOs;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class DepositRequest {
+
+    @NotNull(message = "Sender Account ID cannot be null")
+    public UUID senderId;
+
+    @NotNull(message = "Receiver Account ID cannot be null")
+    public UUID receiverId;
+
+    @NotNull(message = "Amount cannot be blank")
+    @DecimalMin(value = "0.01", message = "Amount must be greater than zero")
+    public BigDecimal amount;
+}
