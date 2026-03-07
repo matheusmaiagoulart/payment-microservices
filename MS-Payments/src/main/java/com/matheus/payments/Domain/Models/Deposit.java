e4d1f51bc3926a198ed7d7c7c1193e1b1f17001b
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-07 12:14:43 -0300

    feat: Implement Deposit entity with status management and validation

---
+++ MS-Payments/src/main/java/com/matheus/payments/Domain/Models/Deposit.java
@@ -0,0 +1,68 @@
+package com.matheus.payments.Domain.Models;
+
+import com.matheus.payments.Domain.Exceptions.InvalidAmountException;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "deposits")
+@NoArgsConstructor
+public class Deposit {
+
+    @Id
+    private UUID depositId;
+    private UUID senderId;
+    private UUID receiverId;
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private DepositStatus status;
+
+    private LocalDateTime payedAt;
+    private LocalDateTime updatedAt;
+
+    public Deposit(UUID senderId, UUID receiverId, BigDecimal amount) {
+        this.depositId = UUID.randomUUID();
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.amount = normalizeAmount(amount);
+        this.status = DepositStatus.PENDING;
+        this.payedAt = LocalDateTime.now();
+        this.updatedAt = null;
+    }
+
+    public enum DepositStatus {
+        PENDING,
+        SENT,
+        CONFIRMED,
+        FAILED
+    }
+
+    private BigDecimal normalizeAmount(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new InvalidAmountException();
+        }
+        return amount.setScale(2, BigDecimal.ROUND_HALF_UP);
+    }
+
+    public void markAsConfirmed() {
+        this.status = DepositStatus.CONFIRMED;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void markAsFailed() {
+        this.status = DepositStatus.FAILED;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void markAsSent() {
+        this.status = DepositStatus.SENT;
+    }
+}
