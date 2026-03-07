ba2701d562ccc8d353ac8dad84014d79c81da0e9
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-07 12:15:43 -0300

    feat: Add DepositCreatedEvent class to encapsulate deposit creation details

---
+++ MS-Payments/src/main/java/com/matheus/payments/Domain/Events/DepositCreatedEvent.java
@@ -0,0 +1,23 @@
+package com.matheus.payments.Domain.Events;
+
+import com.matheus.payments.Domain.Models.Deposit;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+public class DepositCreatedEvent {
+
+    private final UUID depositId;
+    private final UUID senderId;
+    private final UUID receiverId;
+    private final BigDecimal amount;
+
+    public DepositCreatedEvent(Deposit deposit) {
+        this.depositId = deposit.getDepositId();
+        this.senderId = deposit.getSenderId();
+        this.receiverId = deposit.getReceiverId();
+        this.amount = deposit.getAmount();
+    }
+}
