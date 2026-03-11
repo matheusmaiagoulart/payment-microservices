6dd535013166e84258517d512ea2afa6b0672433
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-11 11:53:51 -0300

    feat: Enhance DepositService with auditing and database retry logic

--- MS-Payments/src/main/java/com/matheus/payments/Application/Services/DepositService.java
+++ MS-Payments/src/main/java/com/matheus/payments/Application/Services/DepositService.java
@@ -1,9 +1,12 @@
 package com.matheus.payments.Application.Services;
 
+import com.matheus.payments.Application.Audit.DepositServiceAudit;
 import com.matheus.payments.Domain.Events.DepositCreatedEvent;
 import com.matheus.payments.Domain.Exceptions.DepositNotFound;
 import com.matheus.payments.Domain.Models.Deposit;
 import com.matheus.payments.Domain.Repositories.DepositRepository;
+import com.matheus.payments.Infra.Exceptions.Custom.DataBaseException;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,24 +16,33 @@ import java.util.UUID;
 @Service
 public class DepositService {
 
+    private final DepositServiceAudit audit;
     private final DepositRepository depositRepository;
     private final ApplicationEventPublisher eventPublisher;
 
-    public DepositService(DepositRepository depositRepository, ApplicationEventPublisher eventPublisher) {
+    public DepositService(DepositRepository depositRepository, ApplicationEventPublisher eventPublisher, DepositServiceAudit audit) {
+        this.audit = audit;
         this.depositRepository = depositRepository;
         this.eventPublisher = eventPublisher;
     }
 
     @Transactional
     public void createDeposit(Deposit deposit) {
-        depositRepository.saveDeposit(deposit);
-        eventPublisher.publishEvent(new DepositCreatedEvent(deposit));
+        try {
+            audit.logCreateDepositEntry();
+            depositRepository.saveDeposit(deposit);
+            eventPublisher.publishEvent(new DepositCreatedEvent(deposit));
+        } catch (DataBaseException e) {
+            audit.logErrorCreateDeposit(e.getMessage());
+             throw e;
+        }
     }
 
     public void updateDeposit(Deposit deposit) {
         depositRepository.saveDeposit(deposit);
     }
 
+    @Retry(name = "databaseRetry")
     @Transactional
     public void setDepositStatusSent(String depositId) {
         Deposit deposit = depositRepository.getDepositById(UUID.fromString(depositId))
@@ -40,6 +52,7 @@ public class DepositService {
         updateDeposit(deposit);
     }
 
+    @Retry(name = "databaseRetry")
     @Transactional
     public void setDepositStatusExecuted(String depositId) {
         Deposit deposit = depositRepository.getDepositById(UUID.fromString(depositId))
@@ -49,6 +62,7 @@ public class DepositService {
         updateDeposit(deposit);
     }
 
+    @Retry(name = "databaseRetry")
     @Transactional
     public void setDepositStatusFailed(String depositId) {
         Deposit deposit = depositRepository.getDepositById(UUID.fromString(depositId))
