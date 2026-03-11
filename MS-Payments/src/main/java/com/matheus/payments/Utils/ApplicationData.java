4c6b709366681c1814086ad94414bcd7c57f7af7
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-11 11:56:14 -0300

    feat: Add APPLICATION_CONSUMER_GROUP constant for payments service

--- MS-Payments/src/main/java/com/matheus/payments/Utils/ApplicationData.java
+++ MS-Payments/src/main/java/com/matheus/payments/Utils/ApplicationData.java
@@ -3,4 +3,6 @@ package com.matheus.payments.Utils;
 public class ApplicationData {
 
     public static final String SERVICE_NAME = "MS-InstantPayment";
+
+    public static final String APPLICATION_CONSUMER_GROUP = "payments-service-group";
 }
