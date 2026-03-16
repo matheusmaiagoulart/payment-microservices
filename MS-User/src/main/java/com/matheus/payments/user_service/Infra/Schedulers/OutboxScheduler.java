514f0bd4289befa1fd71efef96d61a274b491f4e
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-16 09:42:31 -0300

    refactor: Integrate OutboxService into OutboxScheduler for event publishing

--- MS-User/src/main/java/com/matheus/payments/user_service/Infra/Schedulers/OutboxScheduler.java
+++ MS-User/src/main/java/com/matheus/payments/user_service/Infra/Schedulers/OutboxScheduler.java
@@ -1,11 +1,6 @@
 package com.matheus.payments.user_service.Infra.Schedulers;
 
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+import com.matheus.payments.user_service.Application.Services.OutboxService;
 import com.matheus.payments.user_service.Domain.Models.Outbox;
 import com.matheus.payments.user_service.Domain.Repositories.OutboxRepository;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -23,44 +18,16 @@ import java.util.List;
 public class OutboxScheduler {
 
     private final OutboxRepository outboxRepository;
-    private final KafkaTemplate<String, String> publisher;
+    private final OutboxService outboxService;
 
-    public OutboxScheduler(OutboxRepository outboxRepository, KafkaTemplate<String, String> publisher) {
+    public OutboxScheduler(OutboxRepository outboxRepository, OutboxService outboxService) {
+        this.outboxService = outboxService;
         this.outboxRepository = outboxRepository;
-        this.publisher = publisher;
     }
 
     @Scheduled(fixedDelay = 6000)
     public void sendToCreateWallet() {
         List<Outbox> pendingToSend = outboxRepository.findAllBySentFalse();
-        pendingToSend.forEach(outbox -> sendOutboxEvent(outbox));
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW) // Always create a new transaction for each outbox event
-    public void sendOutboxEvent(Outbox outbox) {
-        try {
-            Message<String> message = MessageBuilder
-                    .withPayload(outbox.getPayload())
-                    .setHeader("correlationId", outbox.getCorrelationId().toString())
-                    .setHeader(KafkaHeaders.TOPIC, outbox.getTopic())
-                    .build();
-
-            publisher.send(message);
-            setOutboxSent(outbox);
-        } catch (Exception e) {
-            setOutboxFailed(outbox, e.getMessage());
-            outboxRepository.save(outbox);
-        }
-    }
-
-    public void setOutboxSent(Outbox outbox) {
-        outbox.setSent(true);
-        outboxRepository.save(outbox);
-    }
-
-    public void setOutboxFailed(Outbox outbox, String errorMessage) {
-        outbox.setFailed(true);
-        outbox.setFailureReason(errorMessage);
-        outboxRepository.save(outbox);
+        pendingToSend.forEach(outbox -> outboxService.sendOutboxEvent(outbox));
     }
 }
