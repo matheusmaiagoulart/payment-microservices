d2c342567006c580f0bdae477e82ad011ea7bf48
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-16 09:41:12 -0300

    refactor: Update exception handling to use ErrorToSaveOutboxException in GlobalExceptionHandler

--- MS-User/src/main/java/com/matheus/payments/user_service/Infra/Exceptions/Handler/GlobalExceptionHandler.java
+++ MS-User/src/main/java/com/matheus/payments/user_service/Infra/Exceptions/Handler/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.matheus.payments.user_service.Infra.Exceptions.Handler;
 
-import com.matheus.payments.user_service.Domain.Exceptios.InvalidCpfException;
-import com.matheus.payments.user_service.Infra.Exceptions.Custom.OutboxPersistException;
+import com.matheus.payments.user_service.Infra.Exceptions.Custom.ErrorToSaveOutboxException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,8 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(OutboxPersistException.class)
-    public ResponseEntity<HandlerMessage> OutboxPersistException(OutboxPersistException ex) {
+    @ExceptionHandler(ErrorToSaveOutboxException.class)
+    public ResponseEntity<HandlerMessage> OutboxPersistException(ErrorToSaveOutboxException ex) {
         HandlerMessage handlerMessage = new HandlerMessage(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(handlerMessage);
     }
