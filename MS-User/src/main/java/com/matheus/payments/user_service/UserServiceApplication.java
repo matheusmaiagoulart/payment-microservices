85453939c64b4d4f2944f51cc23554983083a0c9
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-16 09:44:37 -0300

    refactor: Remove unused EnableRetry annotation from UserServiceApplication

--- MS-User/src/main/java/com/matheus/payments/user_service/UserServiceApplication.java
+++ MS-User/src/main/java/com/matheus/payments/user_service/UserServiceApplication.java
@@ -3,13 +3,11 @@ package com.matheus.payments.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableKafka
-@EnableRetry
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
