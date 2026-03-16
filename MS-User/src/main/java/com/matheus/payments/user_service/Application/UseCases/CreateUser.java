97257d95fb0243fc7043dbe23ed90b365bd5f595
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-16 09:46:30 -0300

    refactor: Simplify CreateUser class by delegating user creation to UserService

--- MS-User/src/main/java/com/matheus/payments/user_service/Application/UseCases/CreateUser.java
+++ MS-User/src/main/java/com/matheus/payments/user_service/Application/UseCases/CreateUser.java
@@ -1,14 +1,8 @@
 package com.matheus.payments.user_service.Application.UseCases;
 
-import com.matheus.payments.user_service.Application.Audit.CreateUserAudit;
-import org.springframework.context.ApplicationEventPublisher;
+import com.matheus.payments.user_service.Application.DTOs.ResponseCreateUser;
+import com.matheus.payments.user_service.Application.Services.UserService;
 import com.matheus.payments.user_service.Application.DTOs.RequestCreateUser;
-import com.matheus.payments.user_service.Domain.Events.UserCreatedEvent;
-import com.matheus.payments.user_service.Domain.Exceptios.*;
-import com.matheus.payments.user_service.Domain.Models.User;
-import com.matheus.payments.user_service.Infra.Repository.OutboxRepository;
-import com.matheus.payments.user_service.Infra.Repository.UserRepository;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,61 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CreateUser {
 
-    private final CreateUserAudit audit;
-    private final UserRepository userRepository;
-    private final OutboxRepository outboxRepository;
-    private final ApplicationEventPublisher eventPublisher;
+    private final UserService userService;
 
-    public CreateUser(UserRepository userRepository, OutboxRepository outboxRepository, CreateUserAudit audit, ApplicationEventPublisher eventPublisher) {
-        this.audit = audit;
-        this.userRepository = userRepository;
-        this.eventPublisher = eventPublisher;
-        this.outboxRepository = outboxRepository;
-    }
+    public CreateUser(UserService userService) { this.userService = userService; }
 
     @Transactional
-    public String createUser(RequestCreateUser data) {
-
-        audit.logUserCreationStarting(data.getCpf());
-        User user = new User(
-                data.getFirstName(),
-                data.getLastName(),
-                data.getEmail(),
-                data.getCpf(),
-                data.getPhoneNumber(),
-                data.getAccountType(),
-                data.getBirthDate()
-        );
-
-        try {
-            userRepository.saveAndFlush(user);
-
-            UserCreatedEvent userCreatedEvent = new UserCreatedEvent(user.getId(), user.getCpf(), user.getAccountType());
-            eventPublisher.publishEvent(userCreatedEvent);
-
-            audit.logUserCreationSuccess(data.getCpf());
-            return "Congrats! Your request to create a account has been received and is being processed. You will receive a confirmation email shortly.";
-        } catch (DataIntegrityViolationException e) {
-            audit.logUserCreationFailed(data.getCpf(), "Unique field violation: " + e.getMessage());
-            throw handleUniqueFieldException(e);
-        }
+    public ResponseCreateUser execute(RequestCreateUser data) {
+        return userService.createUser(data);
     }
-
-
-    public RuntimeException handleUniqueFieldException(DataIntegrityViolationException e) throws PhoneNumberAlreadyExistsException, CpfAlreadyExistsException, EmailAlreadyExists, UniqueFieldsEntryViolationException {
-        var error = e.getCause().getMessage();
-
-        if (error.contains("uk_users_phone_number")) {
-            throw new PhoneNumberAlreadyExistsException();
-        }
-        if (error.contains("uk_users_cpf")) {
-            throw new CpfAlreadyExistsException();
-        }
-        if (error.contains("uk_users_email")) {
-            throw new EmailAlreadyExists();
-        }
-
-        return new UniqueFieldsEntryViolationException("Unique field violation: " + e.getMessage());
-    }
-
 }
