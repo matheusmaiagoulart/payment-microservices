1e37d3d22bd0a2a5fccff1456477962f26a3f6d7
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-03-16 09:43:03 -0300

    refactor: Update UserCreatedInternalEventHandler to handle events before commit and improve error logging

--- MS-User/src/main/java/com/matheus/payments/user_service/Application/EventHandlers/UserCreatedInternalEventHandler.java
+++ MS-User/src/main/java/com/matheus/payments/user_service/Application/EventHandlers/UserCreatedInternalEventHandler.java
@@ -7,13 +7,8 @@ import com.matheus.payments.user_service.Application.Services.OutboxService;
 import com.matheus.payments.user_service.Domain.Events.UserCreatedEvent;
 import com.matheus.payments.user_service.Utils.ApplicationData;
 import com.matheus.payments.user_service.Utils.KafkaTopics;
-import jakarta.persistence.PersistenceException;
 import lombok.extern.slf4j.Slf4j;
 import org.shared.Logs.LogBuilder;
-import org.springframework.dao.DataAccessException;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Recover;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -37,17 +32,16 @@ public class UserCreatedInternalEventHandler {
         this.objectMapper = objectMapper;
     }
 
-    @Retryable(value = {JsonProcessingException.class, DataAccessException.class, PersistenceException.class}, maxAttempts = 4, backoff = @Backoff(delay = 2000, multiplier = 2))
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void handler(UserCreatedEvent event) throws JsonProcessingException {
+        try {
             String payload = objectMapper.writeValueAsString(event);
-            outboxService.createOutbox(event.getUserId(), "UserCreated", KafkaTopics.USER_CREATED, payload, CorrelationId.get());
-    }
+            outboxService.createOutbox(event.getUserId(), "UserCreated", KafkaTopics.USER_CREATED, payload);
+        } catch (Exception e) {
+            log.error("Failed to process UserCreatedEvent for userId {} after retries", event.getUserId(),
+                    LogBuilder.eventLog("UserCreated", KafkaTopics.USER_CREATED, event.getCpf()),
+                    LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), getClass().getName(), "handler", e.getMessage()));
+        }
 
-    @Recover
-    public void handleRetryFailed(Exception e, UserCreatedEvent event)  {
-        log.error("Failed to process UserCreatedEvent for userId {} after retries", event.getUserId(),
-                LogBuilder.eventLog("UserCreated", KafkaTopics.USER_CREATED, event.getCpf()),
-                        LogBuilder.baseLog(ApplicationData.APPLICATION_NAME, CorrelationId.get(), getClass().getName(), "handler", e.getMessage()));
     }
 }
